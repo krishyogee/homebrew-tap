d49fc5ce5ed3bd0e68e611dd16df2e600fc36668
Yogeeshwaran <yogeeshwaran@trainery.one>
2026-04-30 17:04:10 +0530

    feat: bump gitmate to v0.1.1 (init + first-run banner)

--- Formula/gitmate.rb
+++ Formula/gitmate.rb
@@ -1,28 +1,28 @@
 class Gitmate < Formula
   desc "Multi-agent AI CLI for Git workflows with approval gates"
   homepage "https://github.com/krishyogee/gitmate"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/krishyogee/gitmate/releases/download/v0.1.0/gitmate_0.1.0_darwin_arm64.tar.gz"
-      sha256 "6df3038c9aedcd6bf5a0e2d5eb434c43922a48783adf2e138fe6ddf39a28f336"
+      url "https://github.com/krishyogee/gitmate/releases/download/v0.1.1/gitmate_0.1.1_darwin_arm64.tar.gz"
+      sha256 "c138bbc999cd15f6f90187f99e27d794ff841959fb7dcd17f1125a66f5dd4d48"
     end
     on_intel do
-      url "https://github.com/krishyogee/gitmate/releases/download/v0.1.0/gitmate_0.1.0_darwin_amd64.tar.gz"
-      sha256 "97fb428311b3dc6b27be7af7b3ef87cb48c3f1517cd85bb381d27110dceebc8b"
+      url "https://github.com/krishyogee/gitmate/releases/download/v0.1.1/gitmate_0.1.1_darwin_amd64.tar.gz"
+      sha256 "71a19aa5966cbb47f926e3ead9992460408af5e12161bffdd4e710f9b2a25877"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/krishyogee/gitmate/releases/download/v0.1.0/gitmate_0.1.0_linux_arm64.tar.gz"
-      sha256 "a8758071e63fa87fda14e5e6e2d70133c26bb2e8655bcaa2cc99024d48e51eba"
+      url "https://github.com/krishyogee/gitmate/releases/download/v0.1.1/gitmate_0.1.1_linux_arm64.tar.gz"
+      sha256 "deafb0ce2fd3d0a0a9e25f185dd0b3a040340c4f3814d373d3792d8b17200b6d"
     end
     on_intel do
-      url "https://github.com/krishyogee/gitmate/releases/download/v0.1.0/gitmate_0.1.0_linux_amd64.tar.gz"
-      sha256 "bdd832aa6fab1df81fb1eda98954cbeaab13ce929b6bad7417f2efa13102ac8d"
+      url "https://github.com/krishyogee/gitmate/releases/download/v0.1.1/gitmate_0.1.1_linux_amd64.tar.gz"
+      sha256 "7de13c2782851f4de0eda7a85b202aad02ac1a1ffa8fc6d50b0b260618c3d85d"
     end
   end
 
